Fixing svm gradient bug

assignment1/cs231n/classifiers/linear_svm.py
@@ -28,15 +28,15 @@ def svm_loss_naive(W, X, y, reg):
 	for i in xrange(num_train):
 		scores = X[i].dot(W)
 		correct_class_score = scores[y[i]]
-	for j in xrange(num_classes):
-		margin = scores[j] - correct_class_score + 1 # note delta = 1
-		if margin > 0:
+		for j in xrange(num_classes):
 			if j == y[i]:
-				dW[:,j] -= X[i]
-			else:
-				loss += margin
+				continue
+			margin = scores[j] - correct_class_score + 1 # note delta = 1
+			# print("%d %d %f" % (i,j,margin))
+			if margin > 0:
 				dW[:,j] += X[i]
-
+				dW[:,y[i]] -= X[i]
+				loss += margin
 
 	# Right now the loss is a sum over all training examples, but we want it
 	# to be an average instead so we divide by num_train.
@@ -58,33 +58,6 @@ def svm_loss_naive(W, X, y, reg):
 
 	return loss, dW
 
-# dW = np.zeros(W.shape) # initialize the gradient as zero
-
-# # compute the loss and the gradient
-# num_classes = W.shape[1]
-# num_train = X.shape[0]
-# loss = 0.0
-# for i in xrange(num_train):
-#   scores = X[i].dot(W)
-#   correct_class_score = scores[y[i]]
-#   for j in xrange(num_classes):
-#     margin = scores[j] - correct_class_score + 1 # note delta = 1
-
-		# if margin > 0:
-		#   loss += margin
-		#   if j == y[i]:
-		#     dW[:,j] -= X[i]
-		#   else:
-		#     dW[:,j] += X[i]
-
-# # Right now the loss is a sum over all training examples, but we want it
-# # to be an average instead so we divide by num_train.
-# loss /= num_train
-# dW /= num_train
-
-# # Add regularization to the loss.
-# loss += reg * np.sum(W * W)
-
 
 def svm_loss_vectorized(W, X, y, reg):
 	"""
@@ -100,7 +73,16 @@ def svm_loss_vectorized(W, X, y, reg):
 	# Implement a vectorized version of the structured SVM loss, storing the    #
 	# result in loss.                                                           #
 	#############################################################################
-	pass
+	delta = 1.0
+	scores = W.dot(x)
+	# compute the margins for all classes in one vector operation
+	margins = np.maximum(0, scores - scores[y] + delta)
+	# on y-th position scores[y] - scores[y] canceled and gave delta. We want
+	# to ignore the y-th position and only consider margin on max wrong class
+	margins[y] = 0
+	loss_i = np.sum(margins)
+	return loss_i
+
 	#############################################################################
 	#                             END OF YOUR CODE                              #
 	#############################################################################